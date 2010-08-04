0e8fd3e1fc7acc707846add01f1e626ab3db0cc9
Ralph Meier <ralph_meier@swissonline.ch>
2010-08-04 18:57:57 +0200

    fizzbuzz javascript mit jasmine vorbereitet

---
+++ javascript/fizzbuzz/spec/FizzBuzzSequenceSpec.js
@@ -0,0 +1,14 @@
+describe("FizzBuzzSequence", function() {
+    it('should be a test', function () {
+        var foo = 0;
+        foo++;
+    });
+
+    it('should be a test', function () {
+        var foo = 0;            // set up the world
+        foo++;                  // call your application code
+
+        expect(foo).toEqual(1); // passes because foo == 1
+    });
+
+});
---
+++ javascript/fizzbuzz/spec/FizzBuzzSpec.js
@@ -0,0 +1,17 @@
+describe("FizzBuzz", function() {
+    it('should be a test', function () {
+        var foo = 0;
+        foo++;
+    });
+    it('should be a test', function () {
+        var foo = 0;            // set up the world
+        foo++;                  // call your application code
+
+        expect(foo).toEqual(1); // passes because foo == 1
+    });
+    it('should have 4 in an array with 1,2,3', function () {
+        expect("my Name ist Ralph Meier").not.toContain("T");
+    });
+
+});
+
